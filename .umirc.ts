e6752ea9c1a5106458073dcec9421089ebe21da2
skywalker <houzhenhong@hotmail.com>
2019-09-01 12:17:21 +0800

    chore: add react-spring postcss-px-to-viewport

--- .umirc.ts
+++ .umirc.ts
@@ -1,26 +1,39 @@
-import { IConfig } from 'umi-types';
+import { IConfig } from 'umi-types'
+import pxToViewPort from 'postcss-px-to-viewport'
 
 // ref: https://umijs.org/config/
-const config: IConfig =  {
+const config: IConfig = {
   treeShaking: true,
   publicPath: './',
   history: 'hash',
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: false,
-      dva: false,
-      dynamicImport: false,
-      title: 'departmental-introduction-2019',
-      dll: false,
-      
-      routes: {
-        exclude: [
-          /components\//,
-        ],
+    [
+      'umi-plugin-react',
+      {
+        antd: false,
+        dva: false,
+        dynamicImport: false,
+        title: 'departmental-introduction-2019',
+        dll: false,
+
+        routes: {
+          exclude: [/components\//],
+        },
       },
-    }],
+    ],
+  ],
+  extraPostCSSPlugins: [
+    pxToViewPort({
+      viewportWidth: 375,
+      viewportHeight: 667,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false,
+    }),
   ],
 }
 
-export default config;
+export default config

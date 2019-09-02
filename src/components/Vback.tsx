177ff082140f2431940cab08b4cc6ddf5297e591
skywalker <houzhenhong@hotmail.com>
2019-09-02 22:18:43 +0800

    feat: 返回按钮

--- src/components/Vback.tsx
+++ src/components/Vback.tsx
@@ -1,10 +1,17 @@
 import React, { Fragment } from 'react'
 import styles from './Vback.less'
+import router from 'umi/router';
 
 const VBack: React.FC = () => {
+  const handelBackClick = () => {
+    router.push({
+      pathname: '/'
+    })
+  }
+
   return (
     <Fragment>
-      <div className={styles.content}>
+      <div className={styles.content} onClick={handelBackClick}>
         <span>返回</span>
       </div>
       <div className={styles.divider}>

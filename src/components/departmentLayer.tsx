68117e8f6d67a028f0cbddf9f4490a2b6de98675
skywalker <houzhenhong@hotmail.com>
2019-09-02 20:29:42 +0800

    feat: 完成部门页面加载

--- src/components/departmentLayer.tsx
+++ src/components/departmentLayer.tsx
@@ -1,11 +1,23 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import styles from './departmentLayer.less'
 
 const DepartmentLayer: React.FC = ({ children }) => {
+  const [isProcessed, setProcess] = useState(false)
+  const requestRef: any = React.useRef();
+
+  useEffect(()=>{
+    requestRef.current = setTimeout(()=>{
+      setProcess(true)
+    }, 0)
+    return () => clearTimeout(requestRef.current)
+  }, [])
+
+
   return (
     <Fragment>
-      <div className={styles.backgroundDyeing}></div>
-      <div className={styles.backgroundGrid}></div>
+      <div className={styles.backgroundDyeing}/>
+      <div className={styles.backgroundGrid} style={{ clipPath: isProcessed ? 'inset(0 0 0 0)' : 'inset(0 100% 0 0)' }} />
+      <div className={styles.backgroundOrigin} style={{ clipPath: isProcessed ? 'inset(0 0 0 100%)' : 'inset(0 0 0 0)' }} />
       <div className={styles.children}>{children}</div>
     </Fragment>
   )

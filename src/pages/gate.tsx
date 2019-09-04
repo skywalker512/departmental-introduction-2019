ce3f3021f3381468d0068e9f59e25f345ee6122d
skywalker <houzhenhong@hotmail.com>
2019-09-04 10:49:15 +0800

    feat: 完成门进入提示

--- src/pages/gate.tsx
+++ src/pages/gate.tsx
@@ -5,15 +5,25 @@ import useProcess from '@/utilites/useProcess'
 
 const IndexPage: React.FC = () => {
   const isProcess = useProcess()
+  const isProcessText = useProcess(2000)
   const handelEnterClick = () => {
-    router.push({
-      pathname: '/home'
-    })
+    if (isProcessText) {
+      router.push({
+        pathname: '/home'
+      })
+    }
   }
   return (
     <div>
-      <div className={styles.backGround} style={{ transform: isProcess ? 'scale(1.5)': 'scale(1)' }}/>
-      <div className={styles.text} style={{ opacity: isProcess ? 1: 0 }} onClick={handelEnterClick}> 进入主页 </div>
+      <div className={styles.backGround} style={{ transform: isProcess ? 'scale(1.5)': 'scale(1)' }} onClick={handelEnterClick}/>
+      <div className={styles.text} style={{ opacity: isProcess ? 1: 0 }}>
+        推开这扇大门<br/>
+        我们将带领你解析我们的多元构成
+        <div style={{ opacity: isProcessText ? 1: 0 }}>
+          点击任何地方以进入
+        </div>
+      </div>
+
     </div>
   )
 }

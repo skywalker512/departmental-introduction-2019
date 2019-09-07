c98a07d78c5d44c221c97c56bb49932cba7bcfa7
skywalker <houzhenhong@hotmail.com>
2019-09-07 15:11:30 +0800

    fix: 解决遮挡的问题

--- src/components/homeLayer.tsx
+++ src/components/homeLayer.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import styles from './homeLayer.less'
 const HomeLayer: React.FC = ({ children }) => {
   return (
-    <div className={styles.wapper}>
-      <div className={styles.children}>{children}</div>
-    </div>
+    <Fragment>
+      <div className={styles.wapper}>
+        <div className={styles.children}>
+          <div className={styles.wapper2} />
+          {/*<div className={styles.wapper1} />*/}
+          {children}
+        </div>
+      </div>
+    </Fragment>
   )
 }
 

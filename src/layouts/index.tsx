ec0ee729752c902d74d71455d3db8aef3bad2042
skywalker <houzhenhong@hotmail.com>
2019-09-07 18:29:41 +0800

    feat: 排除动画路由

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,15 +4,18 @@ import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import styles from './index.less'
 
 export default withRouter(({ location, children }) => {
+  const excludeUrl = ['gate']
   return (
     <div className={styles.wapper}>
-      {
+      {excludeUrl.some(item => location.pathname.includes(item)) ? (
+        children
+      ) : (
         <TransitionGroup>
           <CSSTransition key={location.pathname} classNames="page" timeout={300}>
             {children}
           </CSSTransition>
         </TransitionGroup>
-      }
+      )}
     </div>
   )
 })

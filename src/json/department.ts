eeae288ebbd740f6707654cd178692b32eb8d05a
skywalker <houzhenhong@hotmail.com>
2019-09-07 19:40:50 +0800

    fix: 修改文案

--- src/json/department.ts
+++ src/json/department.ts
@@ -7,7 +7,7 @@ const department = [
       '我们是用户需求挖掘者，拥有严谨的逻辑思维能力。结合数据分析，我们连接抽象与实际，设计产品原型，提出产品升级方案。在新媒体平台上展现创意的无限可能。这里是产品策划及运营部，加入我们，开启你大脑的潜在功能！',
     mark: {
       technology: 70,
-      logic: 80,
+      logic: 92,
       taste: 80,
     },
   },
@@ -18,7 +18,7 @@ const department = [
     detail:
       '我们的舞台很大，灰白的原型图由我们渲染成色，把稍纵即逝的灵感，变成触手可及的样子。作为产品的美化师和设计师，我们专注于移动PC两端产品界面每一像素的绘制、建模和优化。这里是视觉设计部，视觉盛宴等你加入！',
     mark: {
-      technology: 68,
+      technology: 80,
       logic: 70,
       taste: 92,
     },
@@ -31,8 +31,8 @@ const department = [
       '我们与时俱进，不是沉默的代码机器。移动开发部负责移动端产品的开发，以 Java & Kotlin，Objective-C & Swift为主要开发语言，Android，iOS两个平台任你钻研。在这里，移动开发部，用移动应用实现触手可及！',
     mark: {
       technology: 88,
-      logic: 78,
-      taste: 60,
+      logic: 92,
+      taste: 68,
     },
   },
   {
@@ -43,8 +43,8 @@ const department = [
       'web研发部分为前端和后端两个方向。前端热衷于用html、css、JavaScript等将视觉的创意以代码形式转化为交互良好的网站页面；后端负责将产品现实需求转化为清晰的数据模型与业务逻辑以及实现平台的稳定性。',
     mark: {
       technology: 92,
-      logic: 87,
-      taste: 70,
+      logic: 88,
+      taste: 60,
     }
   },
   {
@@ -54,9 +54,9 @@ const department = [
     detail:
       '白帽子黑客是我们的选择，我们是一群神秘又有趣的技术宅。运维安全部负责维护网校的服务器稳定和安全，对新项目的部署、环境安全配置和性能优化，重庆邮电大学开源镜像站。从Linux到系统架构，Python是我们的秘密武器。',
     mark: {
-      technology: 89,
-      logic: 84,
-      taste: 60,
+      technology: 88,
+      logic: 92,
+      taste: 71,
     }
   },
 ]

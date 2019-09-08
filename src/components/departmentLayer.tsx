da478569970e9c136a2478a75e1f808dfb5801da
skywalker <houzhenhong@hotmail.com>
2019-09-08 09:56:38 +0800

    feat: 详情页文字延迟加载

--- src/components/departmentLayer.tsx
+++ src/components/departmentLayer.tsx
@@ -3,24 +3,38 @@ import styles from './departmentLayer.less'
 import useProcess from '@/utilites/useProcess'
 
 const DepartmentLayer: React.FC = ({ children }) => {
-  const [isShow, setShow] = useState(false)
-  const setTimeoutRef: any = React.useRef();
-
-  React.useEffect(() => {
-    setTimeoutRef.current = setTimeout(()=>{
-      setShow(true)
-    }, 700)
-    return () => {
-      clearTimeout(setTimeoutRef.current)
-    };
-  }, []);
+  const isShow = useProcess(1000)
+  const isShowDetal = useProcess(1500)
+  if (isShowDetal) {
+    sessionStorage.setItem('isWordShow', 'true')
+  } else {
+    sessionStorage.setItem('isWordShow', 'false')
+  }
 
   return (
     <Fragment>
-      <div className={styles.backgroundDyeing}/>
-      <div className={styles.backgroundGrid} style={{ clipPath: isShow ? 'inset(0 0 0 0)' : 'inset(0 100% 0 0)', WebkitClipPath:  isShow ? 'inset(0 0 0 0)' : 'inset(0 100% 0 0)' }} />
-      <div className={styles.backgroundOrigin} style={{ clipPath: isShow ? 'inset(0 0 0 100%)' : 'inset(0 0 0 0)', WebkitClipPath: isShow ? 'inset(0 0 0 100%)' : 'inset(0 0 0 0)' }} />
-      { isShow ? <div className={styles.children}>{children}</div> : '' }
+      <div className={styles.backgroundDyeing} />
+      <div
+        className={styles.backgroundGrid}
+        style={{
+          clipPath: isShow ? 'inset(0 0 0 0)' : 'inset(0 100% 0 0)',
+          WebkitClipPath: isShow ? 'inset(0 0 0 0)' : 'inset(0 100% 0 0)',
+        }}
+      />
+      <div
+        className={styles.backgroundOrigin}
+        style={{
+          clipPath: isShow ? 'inset(0 0 0 100%)' : 'inset(0 0 0 0)',
+          WebkitClipPath: isShow ? 'inset(0 0 0 100%)' : 'inset(0 0 0 0)',
+        }}
+      />
+      {isShow ? (
+        <div className={styles.children} style={{ opacity: isShowDetal ? 1 : 0 }}>
+          {children}
+        </div>
+      ) : (
+        ''
+      )}
     </Fragment>
   )
 }

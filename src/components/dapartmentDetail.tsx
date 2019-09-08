55ecda14a66a3111224a8c3ebe9dddd94bbb6089
skywalker <houzhenhong@hotmail.com>
2019-09-08 09:56:51 +0800

    feat: 详情页文字延迟加载

--- src/components/dapartmentDetail.tsx
+++ src/components/dapartmentDetail.tsx
@@ -4,7 +4,11 @@ import useWord from '@/utilites/useWord'
 
 const DepartmentDetail: React.FC = ({ children }) => {
   const P: React.FC<{ text: string }> = ({text}) => {
-    const pText = useWord(text)
+    let delay = 1700
+    if (sessionStorage.getItem('isWordShow') === 'true') {
+      delay = 0
+    }
+    const pText = useWord(text, 0.2, delay)
     return (<Fragment>
       <p style={{ opacity: pText.length < text.length ? 1 : 0, position: 'absolute' }}>{pText}</p>
       <p style={{ opacity: pText.length < text.length ? 0 : 1 }}>{text}</p>
